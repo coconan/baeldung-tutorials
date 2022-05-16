dfe98c11803ca622b026d42a30eedf1c87653afe
brian.yang <brian.yang@huolala.cn>
2022-05-16 23:03:47 +0800

    hack spring mybatis

--- persistence-modules/spring-mybatis/src/main/java/com/baeldung/mybatis/spring/PersistenceConfig.java
+++ persistence-modules/spring-mybatis/src/main/java/com/baeldung/mybatis/spring/PersistenceConfig.java
@@ -3,8 +3,8 @@ package com.baeldung.mybatis.spring;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("com.baeldung.mybatis")
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class })
+//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class })
 public class PersistenceConfig {
 
     @Bean
